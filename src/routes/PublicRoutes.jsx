e430d7ef243e7777228e460dd424e01e0f4dae44
Diego <diego.mercado1997@hotmail.com>
2026-02-27 18:26:00 -0300

    borro

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 export const PublicRoutes = ({ children, isLogged }) => {
 
-  const path = localStorage.getItem('lastRoute') || '/products'; 
+  const path = localStorage.getItem('lastRoute') || '/profile'; 
 
   return (!isLogged)
     ? children
